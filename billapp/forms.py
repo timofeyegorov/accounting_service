98d928b8a77396837fc99b02fd996dad7c5fe49f
timofeyegorov <timofeyegorov48@gmail.com>
2021-10-10 21:27:12 +0700

    new pages form+

--- billapp/forms.py
+++ billapp/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Operation
 
-class OperationForm(ModelForm):
+class OperationForm(forms.ModelForm):
+    operation_sum = forms.FloatField(label='', widget=forms.NumberInput(attrs={'placeholder': 'Сумма', 'class': 'form-control'}))
+    people_name = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Имя', 'class': 'form-control'}))
+    operation_type = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Тип операции', 'class': 'form-control'}))
+    operation_tag = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Назначение', 'class': 'form-control'}))
     class Meta:
         model = Operation
         fields = '__all__'
